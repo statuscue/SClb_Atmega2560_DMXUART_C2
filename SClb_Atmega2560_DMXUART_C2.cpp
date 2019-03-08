c9685ee287576f89c838634efbb05cfa19f2ec93
Eugene Mikhaylov <statuscue@hes.od.ua>
2019-03-08 16:19:36 +0200

    Add project files.

---
+++ SClb_Atmega2560_DMXUART_C2.cpp
@@ -0,0 +1,10 @@
+/*
+ Name:		SClb_Atmega2560_DMXUART_C2.cpp
+ Created:	08-Mar-19 16:19:33
+ Author:	statu
+ Editor:	http://www.visualmicro.com
+*/
+
+#include "SClb_Atmega2560_DMXUART_C2.h"
+
+
---
+++ SClb_Atmega2560_DMXUART_C2.h
@@ -0,0 +1,19 @@
+/*
+ Name:		SClb_Atmega2560_DMXUART_C2.h
+ Created:	08-Mar-19 16:19:33
+ Author:	statu
+ Editor:	http://www.visualmicro.com
+*/
+
+#ifndef _SClb_Atmega2560_DMXUART_C2_h
+#define _SClb_Atmega2560_DMXUART_C2_h
+
+#if defined(ARDUINO) && ARDUINO >= 100
+	#include "arduino.h"
+#else
+	#include "WProgram.h"
+#endif
+
+
+#endif
+
